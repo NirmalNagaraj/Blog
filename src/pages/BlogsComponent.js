3e44a36e9fe66e384128767d47cce3d5af7ef6a5
NirmalNagaraj <nirmalns@snaptechnologies.in>
2024-05-18 19:19:04 +0530

    Responsive

--- src/pages/BlogsComponent.js
+++ src/pages/BlogsComponent.js
@@ -2,67 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import baseURL from '../api/url';
-
-const styles = {
-  bcContainer: {
-    padding: '60px',
-  },
-  bcCardsContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    maxWidth: '800px',
-    margin: '0 auto',
-  },
-  bcCard: {
-    width: 'calc(50% - 20px)',
-    maxWidth: '400px',
-    maxHeight: '400px',
-    border: '1px solid #ccc',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '20px',
-    boxSizing: 'border-box',
-    overflow: 'hidden',
-    cursor: 'pointer',
-  },
-  bcCardImage: {
-    width: '100%',
-    height: 'auto',
-    marginBottom: '10px',
-    borderRadius: '5px',
-    objectFit: 'cover',
-  },
-  bcDescription: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    display: '-webkit-box',
-    WebkitLineClamp: 3,
-    WebkitBoxOrient: 'vertical',
-    marginBottom: 0,
-    textAlign: 'left',
-    
-  },
-  bcPostedOn: {
-    textAlign: 'left',
-    fontStyle: 'italic',
-    marginBottom: '5px',
-  },
-  '@media (max-width: 768px)': { // Target screens smaller than 768px
-    bcPostedOn: {
-      fontSize: 'small', // Adjust font size for smaller screens
-    },
-  },
-  bcTitle: {
-    textAlign: 'left',
-    fontWeight: 'bold',
-    marginBottom: '5px',
-  },
-};
+import './BlogsComponent.css';
 
 const BlogsComponent = () => {
   const [blogs, setBlogs] = useState([]);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -70,7 +16,10 @@ const BlogsComponent = () => {
         const response = await axios.get(`${baseURL}/api/getBlogs`);
         setBlogs(response.data);
       } catch (error) {
+        setError('Error fetching blogs');
         console.error('Error fetching blogs:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -86,17 +35,20 @@ const BlogsComponent = () => {
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
   };
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
   return (
-    <div style={styles.bcContainer}>
-      <div style={styles.bcCardsContainer}>
+    <div className="bc-container">
+      <div className="bc-cards-container">
         {blogs.map(blog => (
-          <div key={blog.id} style={styles.bcCard} onClick={() => navigateToBlogDetail(blog.title)}>
+          <div key={blog.id} className="bc-card" onClick={() => navigateToBlogDetail(blog.title)}>
             {blog.thumbnail && (
-              <img src={`${blog.thumbnail}`} alt="Thumbnail" style={styles.bcCardImage} />
+              <img src={blog.thumbnail} alt="Thumbnail" className="bc-card-image" />
             )}
-            <p style={styles.bcPostedOn}>Posted on: {formatTimestamp(blog.created_at)}</p>
-            <h2 style={styles.bcTitle}>{blog.title}</h2>
-            <p style={styles.bcDescription}>{blog.description}</p>
+            <p className="bc-posted-on">Posted on: {formatTimestamp(blog.created_at)}</p>
+            <h2 className="bc-title">{blog.title}</h2>
+            <p className="bc-description">{blog.description}</p>
           </div>
         ))}
       </div>
